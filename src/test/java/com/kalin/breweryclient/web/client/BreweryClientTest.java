056c57dca476f53fb844b457363092e1c8f62827
KalinDonchev <kalindonchev@mail.bg>
2021-11-11 17:50:39 +0200

    initial commit

---
+++ src/test/java/com/kalin/breweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,48 @@
+package com.kalin.breweryclient.web.client;
+
+import com.kalin.breweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient breweryClient;
+
+    @Test
+    void getBeerById() {
+        BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
+
+        assertNotNull(beerDto);
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+        URI uri = breweryClient.saveNewBeer(beerDto);
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void testUpdateBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+        breweryClient.updateBeer(UUID.randomUUID(),beerDto);
+    }
+
+    @Test
+    void testDeleteBeer(){
+        breweryClient.deleteBeer(UUID.randomUUID());
+    }
+
+}
